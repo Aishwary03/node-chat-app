75bc583d9e517a29a1ae9f625d6d14cab8797613
Aishwary03 <agrawal.aishwary@tcs.com>
2018-07-18 07:24:01 +0530

    Form for messages and show incoming messages

--- server/server.js
+++ server/server.js
@@ -15,13 +15,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('emitMessage', generateMessage('Admin', 'Welcome to the chat App'));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat App'));
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User joined'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This is from server');
     })
 
     socket.on('disconnect', () => {
